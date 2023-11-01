b3614b5e0abc5feff6a1dcb2090fe627ed342496
denervalspelizario <denervalspelizario@gmail.com>
2023-11-01 17:36:00 -0300

    update adicao de header e sua estilização

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,29 @@
+import Image from 'next/image'; // importando Image que trata imagens no next
+import Logo from '../../../public/rockclub.png'
 import styles from './styles.module.css'
+import { BsFacebook, BsTwitter, BsYoutube, BsInstagram } from 'react-icons/bs';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>
-      <h2 className={styles.title}>Header</h2>
-    </div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerMedia}>
+        <Image src={Logo} alt='Programador Logo' className={styles.headerLogo} />
+        <nav className={styles.headerNavMedia}>
+          <Link href="https://www.facebook.com"  target="_blank">
+            <BsFacebook className={styles.socialMedia}/>
+          </Link>
+          <Link href="https://www.facebook.com"  target="_blank">
+            <BsTwitter  className={styles.socialMedia}/>
+          </Link>
+          <Link href="https://www.facebook.com"  target="_blank">
+            <BsYoutube  className={styles.socialMedia}/>
+          </Link>
+          <Link href="https://www.facebook.com"  target="_blank">
+            <BsInstagram  className={styles.socialMedia}/>
+          </Link>
+        </nav>
+      </div>
+    </header>
   )
 }
